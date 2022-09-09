b75703848e14ab2cf21c96dce4a57d65e00718a5
TweakPY <69265406+Filza2@users.noreply.github.com>
2022-09-09 09:21:01 +0300

    Update CVD-23.py

--- CVD-23.py
+++ CVD-23.py
@@ -130,65 +130,69 @@ def Track_CIPM():
     if url_red=='':print(f'{Rc}[{wh}!{Rc}]  {wh}Enter url please ..');time.sleep(0.9);return Track_CIPM()
     if email=='':exit(f'{Rc}[{wh}!{Rc}]  {wh}Please input a Email in config File !!')
     elif '@' not in email:exit(f'{Rc}[{wh}!{Rc}]  {wh}Check your email please and retry !!')
-    d=f"""
------------------------------8386526011242191222501571313
+    d=f"""-----------------------------6640491382465600322232798945
 Content-Disposition: form-data; name="type"
 
 fast_redirect
------------------------------8386526011242191222501571313
+-----------------------------6640491382465600322232798945
 Content-Disposition: form-data; name="email"
 
 {email}
------------------------------8386526011242191222501571313
+-----------------------------6640491382465600322232798945
 Content-Disposition: form-data; name="webhook"
 
 
------------------------------8386526011242191222501571313
+-----------------------------6640491382465600322232798945
 Content-Disposition: form-data; name="fmt"
 
 
------------------------------8386526011242191222501571313
+-----------------------------6640491382465600322232798945
 Content-Disposition: form-data; name="memo"
 
 {url_red}
------------------------------8386526011242191222501571313
+-----------------------------6640491382465600322232798945
+Content-Disposition: form-data; name="cmd_process"
+
+
+-----------------------------6640491382465600322232798945
 Content-Disposition: form-data; name="clonedsite"
 
 
------------------------------8386526011242191222501571313
+-----------------------------6640491382465600322232798945
 Content-Disposition: form-data; name="sql_server_table_name"
 
 TABLE1
------------------------------8386526011242191222501571313
+-----------------------------6640491382465600322232798945
 Content-Disposition: form-data; name="sql_server_view_name"
 
 VIEW1
------------------------------8386526011242191222501571313
+-----------------------------6640491382465600322232798945
 Content-Disposition: form-data; name="sql_server_function_name"
 
 FUNCTION1
------------------------------8386526011242191222501571313
+-----------------------------6640491382465600322232798945
 Content-Disposition: form-data; name="sql_server_trigger_name"
 
 TRIGGER1
------------------------------8386526011242191222501571313
+-----------------------------6640491382465600322232798945
 Content-Disposition: form-data; name="redirect_url"
 
 {url_red}
------------------------------8386526011242191222501571313--"""
-    req=post("https://canarytokens.org/generate",headers={'Host': 'canarytokens.org','User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0','Accept': 'application/json, text/javascript, */*; q=0.01','Accept-Language': 'ar,en-US;q=0.7,en;q=0.3','Accept-Encoding': 'gzip, deflate','X-Requested-With': 'XMLHttpRequest','Content-Type': 'multipart/form-data; boundary=---------------------------8386526011242191222501571313','Content-Length': '1451','Origin': 'https://canarytokens.org','Referer': 'https://canarytokens.org/generate','Te': 'trailers','Connection': 'close'},data=d).json()
+-----------------------------6640491382465600322232798945--
+"""
+    req=post("https://canarytokens.org/generate",headers={'Host': 'canarytokens.com','User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0','Accept': 'application/json, text/javascript, */*; q=0.01','Accept-Language': 'ar,en-US;q=0.7,en;q=0.3','Accept-Encoding': 'gzip, deflate','X-Requested-With': 'XMLHttpRequest','Content-Type': 'multipart/form-data; boundary=---------------------------6640491382465600322232798945','Content-Length': '1559','Origin': 'https://canarytokens.com','Referer': 'https://canarytokens.com/generate','Sec-Fetch-Dest': 'empty','Sec-Fetch-Mode': 'cors','Sec-Fetch-Site': 'same-origin','Te': 'trailers','Connection': 'close'},data=d).json()
     try:
         token=req["Token"];em=req["Email"];url=f'https://canarytokens.com/TweakPY.vv1ck/{token}'
         try:
-            res=post('https://hideuri.com/api/v1/shorten',headers={'Host': 'hideuri.com','Cookie': '_cfvdata=as874as89sa9as84s89f4asfa8f','User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0','Accept': 'application/json, text/javascript, */*; q=0.01','Accept-Language': 'ar,en-US;q=0.7,en;q=0.3','Accept-Encoding': 'gzip, deflate','Referer': 'https://hideuri.com/','Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8','X-Requested-With': 'XMLHttpRequest','Content-Length': '47','Origin': 'https://hideuri.com','Te': 'trailers'},data=f'url={url}')
+            res=post('https://hideuri.com/api/v1/shorten',headers={'Host': 'hideuri.com','Cookie': '_cfvdata=','User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0','Accept': 'application/json, text/javascript, */*; q=0.01','Accept-Language': 'ar,en-US;q=0.7,en;q=0.3','Accept-Encoding': 'gzip, deflate','Referer': 'https://hideuri.com/','Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8','X-Requested-With': 'XMLHttpRequest','Content-Length': '47','Origin': 'https://hideuri.com','Te': 'trailers'},data=f'url={url}')
             if 'result_url' in res.json():shorted_url=res.json()['result_url']
             else:pass
         except:shorted_url='Null'
         print("-------------------------------")
-        print(f'{Rc}[{wh}+{Rc}]  Token: {token}')
-        print(f'{Rc}[{wh}+{Rc}]  url: {url}')#we will send that to the target don't worry.
-        print(f'{Rc}[{wh}+{Rc}]  Shorted url: {shorted_url}')
-        print(f'{Rc}[{wh}+{Rc}]  Email: {em}')
+        print(f'{Rc}[{wh}+{Rc}]  Token : {token}')
+        print(f'{Rc}[{wh}+{Rc}]  url : {url}')#we will send that to the target don't worry.
+        print(f'{Rc}[{wh}+{Rc}]  Shorted url : {shorted_url}')
+        print(f'{Rc}[{wh}+{Rc}]  Email : {em}')
         if shorted_url !='Null':
             if 'hideuri.com' in shorted_url:url=shorted_url
             else:pass
